80878ed9ccdda689d60cceba1fa01a17bbca9e56
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-05-22 20:52:05 +0530

    initialize backend server with Express and dotenv

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import dotenv from "dotenv"
+import DbConnect from './DB/dbConnect.js';
+import authRouter from "./Routes/authRoutes.js"
+
+const app=express()
+
+dotenv.config();
+
+DbConnect()
+
+app.use('/api/auth', authRouter)
+
+
+app.get("/",(req, res)  =>{
+    res.send("server is working")
+})
+
+
+
+
+
+const PORT=process.env.PORT ||3000
+app.listen(PORT,()=>{
+    console.log(`server running at ${PORT}`);
+    
+})
