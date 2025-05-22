8977b76428738746861c4a5b9a39abceb17bd5cd
siddharth rukadikar <siddharthrukadikar@gmail.com>
2025-05-22 20:54:13 +0530

    add db connect func

---
+++ backend/DB/dbConnect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const DbConnect=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URI),
+        console.log("mongoDb connected successfully");
+        
+    } catch (error) {
+        console.log(error());
+        
+    }
+}
+
+export default DbConnect
